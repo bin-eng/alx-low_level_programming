7a787b59a7f319ca82d4305101cc68265ef27fe3
Binyam Tesfaye <binyam275@gmail.com>
2022-12-21 11:26:52 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Binyam Tesfaye
+*/
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+char ch;
+for (letter = a; letter <= z; letter++)
+{
+if (letter != e && letter != q)
+putchar(letter);
+}
+putchar(n);
+return (0);
+}
+
