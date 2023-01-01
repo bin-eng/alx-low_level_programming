591844ca8b65595b37494df6e1c4b80501b4e54c
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 00:27:18 +0300

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "../main.h"
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ * Return: Always 0.
+ */
+int main (void)
+{
+  void positive_or_negative(int i);
+  return (0);
+}
