af177e1f16502b6e0c9eee3604369fba17562a22
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 00:35:01 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,6 +7,19 @@
  */
 int main (void)
 {
-  positive_or_negative();
-  return (0);
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+  if (n > 0)
+    printf("%d is positive\n", n);
+
+  else if (n < 0)
+    printf("%d is negative\n", n);
+
+  else
+    printf("%d is zero\n", n);
+
+return (0);
 }
