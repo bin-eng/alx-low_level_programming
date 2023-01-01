e74b6e2ba5367e4b0ed29a43f5720c534a7211a0
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 01:25:02 +0300

    Print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,16 @@
  */
 int main()
 {
-  char letter;
-  for(letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-  for(letter = 'A'; letter <= 'Z'; letter++)
-    putchar(letter);
-  putchar('\n');
-  
-  return (0);
+char letter;
+
+for(letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+for(letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
 }
 
