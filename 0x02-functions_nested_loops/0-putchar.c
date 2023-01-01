f08d83db59d6c71aaa67272240c5320664940a11
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-01 20:53:22 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
  * File: 0-ptchar.c
  * Auth: Binyam Tesfaye
  */
-#include <stdio.h>
+#include "main.h"
 int main(void)
 {
- _putchar(_);
- _putchar(p);
- _putchar(u);
- _putchar(t);
- _putchar(c);
- _putchar(h);
- _putchar(a);
- _putchar(r);
- _putchar(n);
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('n');
 
  return (0);
 }
