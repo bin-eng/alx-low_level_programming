35e750c7234cd5c200a1a038d2e6df63b3227ece
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 02:57:40 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
  * Return: Always 0.
  */
 int main(void)
