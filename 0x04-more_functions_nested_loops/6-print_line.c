83b943bd22cdab163411919f368c4ca3d38c358e
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 04:23:58 +0300

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Binyam Tesfaye
+ */
+#include "main.h"
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+int len;
+if (n > 0)
+{
+for (len = 0;
+_putchar('_');
+}
+_putchar('\n');
+}
