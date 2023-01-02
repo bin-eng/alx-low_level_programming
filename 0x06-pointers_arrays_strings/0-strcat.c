ca42690aeeda92075e405788bdb11e03a50c29ed
Binyam Tesfaye <84023144+bin-eng@users.noreply.github.com>
2023-01-02 23:20:45 +0300

    STR cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@
  * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 int index = 0, dest_len = 0;
 
